962b54a6eb64cfca3d70ffa4b5b4e473927c36a1
2m-github <limin1403@gmail.com>
2020-09-03 13:29:12 +0900

    123123123123

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,7 +12,8 @@
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+          Welcome to Vuetify 
+          12312312312312
         </h1>
 
         <p class="subheading font-weight-regular">
