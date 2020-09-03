5b32400261b974dcca544c4b5bfc9c530fd491c7
2m-github <limin1403@gmail.com>
2020-09-03 14:11:54 +0900

    수정

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,8 +13,6 @@
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">
           Welcome to Vuetify 
-          12312312312312
-          123123
         </h1>
 
         <p class="subheading font-weight-regular">
